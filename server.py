73ef623ef481b9195c1bbef22ce098faa1f3a89e
Filippo Barchetti <filippo.barchetti@fermi.mo.it>
2026-01-14 19:06:03 +0100

    Ora il server è completamente asincrono e non più basato sui thread. Questo per rendere il codice più pulito ed evitare problemi mischiando programmazione concorrente e asincrona. Vi è un metodo della classe WSHandler chiamato manager che crea degli oggetti Match, ognuno che si occupa di una partita.

--- server.py
+++ server.py
@@ -1,15 +1,5 @@
-#import sys
 import asyncio
-import threading
 import random
-import time
-
-# Ho dovuto specificare questa condizione perchè
-# altrimenti, usando Windows, vi erano problemi
-"""if sys.platform.startswith("win"):
-    from asyncio import WindowsSelectorEventLoopPolicy
-    asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())"""
-
 import logging
 import tornado.web
 import tornado.websocket
@@ -22,6 +12,8 @@ from file_config import simulation_speed, teams, general, l_teams
 l_winners = l_teams.copy()
 connected_clients = set()
 
+running_tasks = []
+concluded_matches = 0
 
 def broadcast_message(message):
     """Invia un messaggio JSON a tutti i client connessi"""
@@ -32,87 +24,78 @@ def broadcast_message(message):
             logging.error(f"Errore invio: {e}")
     print("Invio")
 
+class Match():
+    def __init__(self, n, team1, team2):
+        self.team1 = team1
+        self.team2 = team2
+        self.name = f"Match {n}"
+        self.loop = True
+        self.seconds = 0
+
+    async def simulate(self):
+        while self.loop:
+            payload = {
+                "time": f"{self.seconds // 60}:{self.seconds % 60}",
+                "teams": f"{self.team1} vs {self.team2}",
+                "new": "no"
+            }
+            broadcast_message(payload)
+            await asyncio.sleep(simulation_speed)
+            print(payload, f"match {self.team1} vs {self.team2}")
+            self.seconds += 1
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("main.html")
 
 
 class WSHandler(tornado.websocket.WebSocketHandler):
+    task_manager = ""
+    task_match_simulator = ""
+    loop_manager = False
+    loop_match_simulator = False
+    manager_is_blocked = False
+    n_teams = 16
+
     def check_origin(self, origin):
         return True
 
     def open(self):
         connected_clients.add(self)
         print("WebSocket aperto")
+        self.loop_manager = True
+        self.task_manager = asyncio.create_task(self.manager())
+
+    async def manager(self):
+        global l_winners, concluded_matches
+        while self.loop_manager:
+            if not self.manager_is_blocked:
+                self.n_teams //= 2
+                for i in range(self.n_teams):
+                    team1 = l_winners.pop(random.randrange(len(l_winners)))
+                    team2 = l_winners.pop(random.randrange(len(l_winners)))
+                    print(f"MasterThread match {i}   n_threads: {self.n_teams}, team1: {team1}, team2: {team2}")
+                    await self.write_message({"teams": f"{team1} vs {team2}", "new": "yes"})
+                    match = Match(i + 1, team1, team2)
+                    self.task_match_simulator = asyncio.create_task(match.simulate())
+                    print(f"Avviata task {i}")
+                    self.manager_is_blocked = True
+
+            else:
+                # Conclusa una fase del campionato
+                if concluded_matches == self.n_teams//2:
+                    self.manager_is_blocked = False
+                # Concluso campionato
+                if self.n_teams == 2:
+                    l_winners = l_teams.copy()
+                    concluded_matches = 0
+                    self.manager_is_blocked = False
+
 
 
     def on_close(self):
         connected_clients.discard(self)
         print("WebSocket chiuso")
-
-
-class MatchRandomizerThread(threading.Thread):
-    def __init__(self, i, stop_event, team1, team2):
-        super().__init__(name=f"match_{i}")
-        self.stop_event = stop_event
-        self.team1 = team1
-        self.team2 = team2
-        self.seconds = 0
-
-    def run(self):
-        while not self.stop_event.is_set():
-            payload = {
-                "time": f"{self.seconds//60}:{self.seconds%60}",
-                "teams": f"{self.team1} vs {self.team2}",
-                "new": "no"
-            }
-            broadcast_message(payload)
-
-            time.sleep(simulation_speed)
-            print(payload, f"match {self.team1} vs {self.team2}")
-            self.seconds += 1
-
-
-class MasterThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self.stop_master_event = stop_master_event
-        self.stop_simulation_event = stop_simulation_event
-
-    def run(self):
-        global l_winners
-        n_threads = 16
-        blocked = False
-        while not self.stop_master_event.is_set():
-            if not blocked:
-                n_threads //= 2
-                print(f"n_threads: {n_threads}")
-                for i in range(n_threads):
-                    team1 = l_winners.pop(random.randrange(len(l_winners)))
-                    team2 = l_winners.pop(random.randrange(len(l_winners)))
-                    print(f"MasterThread match {i}   n_threads: {n_threads}, team1: {team1}, team2: {team2}")
-                    broadcast_message({"teams": f"{team1} vs {team2}", "new": "yes"})
-                    t_match = MatchRandomizerThread(
-                        i + 1,
-                        stop_simulation_event,
-                        team1,
-                        team2
-                    )
-                    t_match.start()
-                    print(f"Avviato thread {i}")
-
-                # Campionato finito, riparto da capo
-                if n_threads == 2:
-                    n_threads = 16
-                l_winners = l_teams
-
-                print("blocco thread master")
-                blocked = True
-            else:
-                print("bloccato")
-                if len(threading.enumerate()) == 2:
-                    blocked = False
-
 
 
 """ Main """
@@ -130,13 +113,8 @@ async def main():
     app.listen(8000)
     print("Server Tornado avviato su http://localhost:8000")
 
-    MasterThread().start()
-    print("AvviaTO MasterThread")
-
     await asyncio.Event().wait()
 
 
 if __name__ == "__main__":
-    stop_master_event = threading.Event()
-    stop_simulation_event = threading.Event()
     asyncio.run(main())
